41da2df0ff43dbacd2551039da442af67d5e1a99
Sebastian González <sebasguuuns@gmail.com>
2025-11-15 17:31:10 -0400

    Update db.py

--- db/db.py
+++ db/db.py
@@ -4,4 +4,20 @@ db = sqlite3.connect('laboratorio.db')
 cursor = db.cursor()
 
 def CrearTablaRegistro():
-    pass
+    query = """
+            CREATE TABLE IF NOT EXISTS Registro( 
+                id_Usuarios INTEGER PRIMARY KEY AUTOINCREMENT,
+                Nombre CHAR(12) NOT NULL,
+                Apellido CHAR(12) NOT NULL,
+                Cedula CHAR(10) NOT NULL UNIQUE,
+                Contraseña CHAR(16) NOT NULL,
+                Fecha_nacimiento CHAR(10) NOT NULL,
+                Correo CHAR(30) NOT NULL,
+                rol CHAR(30) NOT NULL,
+                nivel_autorizacion INTEGER NOT NULL
+            );
+        """  
+    cursor.executescript(query)
+    print(query)
+
+    db.commit

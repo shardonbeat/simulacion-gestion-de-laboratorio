3cc07c509c70c179f27ff5ac756ca3403415cb8c
Sebastian González <sebasguuuns@gmail.com>
2025-11-16 17:10:16 -0400

    Update Frame_Registro.py

--- Interfaces/Frame_Registro.py
+++ Interfaces/Frame_Registro.py
@@ -14,40 +14,45 @@ class frame_registro(CTkFrame):
         self.label_nombre = CTkLabel(self, text="Nombre", font=("Arial", 14), text_color="#000000")
         self.label_nombre.place(x=80, y=70)
 
-        self.entry = CTkEntry(self, width=150,)
-        self.entry.place(x=80, y=100)
+        self.nombre = CTkEntry(self, width=150,)
+        self.nombre.place(x=80, y=100)
 
         self.label_apellido = CTkLabel(self, text="Apellido", font=("Arial", 14), text_color="#000000")
         self.label_apellido.place(x=260, y=70)
 
-        self.entry2 = CTkEntry(self, width=150,)
-        self.entry2.place(x=260, y=100)
+        self.apellido = CTkEntry(self, width=150,)
+        self.apellido.place(x=260, y=100)
 
         self.label_cedula = CTkLabel(self, text="Cédula", font=("Arial", 14), text_color="#000000")
         self.label_cedula.place(x=80, y=140)
 
-        self.entry3 = CTkEntry(self, width=150,)
-        self.entry3.place(x=80, y=170)
+        self.cedula = CTkEntry(self, width=150,)
+        self.cedula.place(x=80, y=170)
 
         self.fecha_nacimiento = CTkLabel(self, text="Fecha de Nacimiento", font=("Arial", 14), text_color="#000000")
         self.fecha_nacimiento.place(x=260, y=140)
 
-        self.entry4 = CTkEntry(self, width=150, placeholder_text="YYYY-MM-DD")
-        self.entry4.place(x=260, y=170)
+        self.fecha_nacimiento = CTkEntry(self, width=150, placeholder_text="YYYY-MM-DD")
+        self.fecha_nacimiento.place(x=260, y=170)
 
         self.label_correo = CTkLabel(self, text="Correo Electrónico", font=("Arial", 14), text_color="#000000")
         self.label_correo.place(x=80, y=210)
 
-        self.entry5 = CTkEntry(self, width=150,)
-        self.entry5.place(x=80, y=240)
+        self.correo = CTkEntry(self, width=150,)
+        self.correo.place(x=80, y=240)
 
         self.nivel_autorizacion = CTkLabel(self, text="Nivel de Autorización", font=("Arial", 14), text_color="#000000")
         self.nivel_autorizacion.place(x=260, y=210)
 
-        self.entry6 = CTkEntry(self, width=150,)
-        self.entry6.place(x=260, y=240)
+        self.nivel_autorizacion_entry = CTkEntry(self, width=150,)
+        self.nivel_autorizacion_entry.place(x=260, y=240)
 
-        Atras = CTkButton(self, text="Cerrar", command=self.destroy)
+        Registrarse = CTkButton(self, text="Registrarse")
+        Registrarse.configure(fg_color="#625B66")
+        Registrarse._hover_color = "#7A7282"
+        Registrarse.place(x=180, y=470)
+
+        Atras = CTkButton(self, text="Atrás", command=self.destroy)
         Atras.configure(fg_color="#625B66")
         Atras._hover_color = "#7A7282"
         Atras.place(x=180, y=520)

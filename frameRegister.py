e9b5549b022f03b7b0b55fb6b7a727272333cf22
Sebastian González <sebasguuuns@gmail.com>
2025-11-25 19:04:02 -0400

    Resetear

--- frameRegister.py
+++ frameRegister.py
@@ -290,6 +290,8 @@ class frameRegister (CTkFrame):
         self.limpiar_errores()
         self.limpiar_campos()
         self.main.frameRegister.place_forget()
+        go = frameRegister(self.main.ventana, self.main)
+        self.main.frameRegister = go
         self.main.frameLogin.place(
             relx=0, rely=0,
             relwidth=1, relheight=1

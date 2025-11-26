d93b2704bc8b45235a971df0abd6419fe0c69a8e
Sebastian González <sebasguuuns@gmail.com>
2025-11-26 06:48:02 -0400

    admin frameees

---
+++ AdministradorGenerarReportes.py
@@ -0,0 +1,51 @@
+from customtkinter import *
+
+class GenerarReportes(CTkFrame):
+    def __init__(self, master, main):
+        super().__init__(master)
+        self.main = main
+        self.configure(
+            fg_color="#2b2b39",
+            corner_radius=0,
+        )
+
+        self.labelFondo = CTkLabel(
+			master=self,
+			text="",
+			fg_color="#2b2b39",
+			bg_color="#A79D8A",
+			corner_radius=15
+		)
+        self.labelFondo.place(
+			relx=0, rely=0,
+			relwidth=1, relheight=1
+		)
+
+        self.labelContenido = CTkLabel(
+			master=self,
+			text="Generación De Reportes",
+			text_color="#ffffff",
+			fg_color="#2b2b39",
+			bg_color="#2b2b39",
+			font=("Times New Roman", 20, "bold"), corner_radius=15,
+		)
+        self.labelContenido.place(
+			relx=0.5, rely=0.12,
+			relwidth=0.4, relheight=0.2,
+			anchor=CENTER
+		)
+
+        self.botonRegistrar = CTkButton(
+            master=self,
+            text="Generar Reporte",
+            hover_color="#4a4a5e",
+            text_color="#ffffff",
+            fg_color="#5e5e72",
+            bg_color="#2b2b39",
+            font=("Times New Roman", 15, "bold"),
+            corner_radius=10
+        )
+        self.botonRegistrar.place(
+            relx=0.35, rely=0.8,
+            relwidth=0.3, relheight=0.07
+        )
--- frameAdmin.py
+++ frameAdmin.py
@@ -1,6 +1,12 @@
 from customtkinter import *
 from PIL import Image
 
+from AdministradorAuditarAcciones import AuditarAcciones
+from AdministradorDefinirPoliticas import DefinirPoliticas
+from AdministradorExportarInformes import ExportarInformes
+from AdministradorGenerarReportes import GenerarReportes
+from AdministradorMejoras import MejorasCierres
+
 class frameAdmin (CTkFrame):
 	def __init__(self, master, main):
 		super().__init__(master)
@@ -9,6 +15,12 @@ class frameAdmin (CTkFrame):
 			fg_color = "#A79D8A",
 			corner_radius = 0
 		)
+
+		self.DefinirPoliticas = DefinirPoliticas(self.main.ventana, self.main)
+		self.GenerarReportes = GenerarReportes(self.main.ventana, self.main)
+		self.AuditarAcciones = AuditarAcciones(self.main.ventana, self.main)
+		self.ExportarInformes = ExportarInformes(self.main.ventana, self.main)
+		self.MejorasCierres = MejorasCierres(self.main.ventana, self.main)
 
 		## Labels
 		self.labelWelcome = CTkLabel(
@@ -112,7 +124,8 @@ class frameAdmin (CTkFrame):
 			bg_color= "#2b2b39",
 			font = ("Times New Roman", 15, "bold"),
 			compound="top",
-			image=self.PoliticasImage
+			image=self.PoliticasImage,
+			command=lambda: self.frame_manager(1)
 		)
 		self.Politicas.place(
 			relx=0.33, rely=0.25,	
@@ -132,7 +145,8 @@ class frameAdmin (CTkFrame):
 			bg_color= "#2b2b39",
 			font = ("Times New Roman", 15, "bold"),
 			compound="top",
-			image=self.ReportesImage
+			image=self.ReportesImage,
+			command=lambda: self.frame_manager(2)
 		)
 		self.Reportes.place(
 			relx=0.53, rely=0.25,
@@ -152,7 +166,8 @@ class frameAdmin (CTkFrame):
 			bg_color= "#2b2b39",
 			font = ("Times New Roman", 15, "bold"),
 			compound="top",
-			image=self.AccionesImage
+			image=self.AccionesImage,
+			command=lambda: self.frame_manager(3)
 		)
 		self.Acciones.place(
 			relx=0.73, rely=0.25,
@@ -163,7 +178,7 @@ class frameAdmin (CTkFrame):
 			Image.open("Img/ExportarInformes.png"),
 			size = (64, 64)
 		)
-		self.ExportarInformes = CTkButton(
+		self.Exportar_Informes = CTkButton(
 			master = self, text = "Exportar informes",
 			text_color = "#ffffff",
 			hover_color="#898995",
@@ -171,18 +186,19 @@ class frameAdmin (CTkFrame):
 			bg_color= "#2b2b39",
 			font = ("Times New Roman", 15, "bold"),
 			compound="top",
-			image=self.ExportarImage
+			image=self.ExportarImage,
+			command=lambda: self.frame_manager(4)
 		)
-		self.ExportarInformes.place(
+		self.Exportar_Informes.place(
 			relx=0.43, rely=0.5,
 			relwidth=0.15, relheight=0.2
 		)
 
-		self.HistorialImage = CTkImage(
+		self.MejorasImage = CTkImage(
 			Image.open("Img/Mejoras.png"),
 			size = (64, 64)
 		)
-		self.MejorasCierres = CTkButton(
+		self.Mejoras_Cierres = CTkButton(
 			master = self, text = "Mejoras o cierres",
 			text_color = "#ffffff",
 			hover_color="#898995",
@@ -190,9 +206,10 @@ class frameAdmin (CTkFrame):
 			bg_color= "#2b2b39",
 			font = ("Times New Roman", 15, "bold"),
 			compound="top",
-			image=self.HistorialImage
+			image=self.MejorasImage,
+			command=lambda: self.frame_manager(5)
 		)
-		self.MejorasCierres.place(
+		self.Mejoras_Cierres.place(
 			relx=0.63, rely=0.5,
 			relwidth=0.15, relheight=0.2
 		)
@@ -211,7 +228,44 @@ class frameAdmin (CTkFrame):
 			relwidth=0.08, relheight=0.04
 		)
 
+	def removerfunciones(self):
+		self.DefinirPoliticas.place_forget()
+		self.GenerarReportes.place_forget()
+		self.AuditarAcciones.place_forget()
+		self.ExportarInformes.place_forget()
+		self.MejorasCierres.place_forget()
+
+	def	frame_manager(self, frame):
+		self.removerfunciones()
+
+		if frame == 1:
+			self.DefinirPoliticas.place(
+				relx=0.25, rely=0.12,
+				relwidth=0.7, relheight=0.75
+			)
+		elif frame == 2:
+			self.GenerarReportes.place(
+				relx=0.25, rely=0.12,
+				relwidth=0.7, relheight=0.75
+			)
+		elif frame == 3:
+			self.AuditarAcciones.place(
+				relx=0.25, rely=0.12,
+				relwidth=0.7, relheight=0.75
+			)
+		elif frame == 4:
+			self.ExportarInformes.place(
+				relx=0.25, rely=0.12,
+				relwidth=0.7, relheight=0.75
+			)
+		elif frame == 5:
+			self.MejorasCierres.place(
+				relx=0.25, rely=0.12,
+				relwidth=0.7, relheight=0.75
+			)
+
 	def volver_main(self):
+		self.removerfunciones()
 		self.main.frameAdmin.place_forget()
 		go = frameAdmin(self.main.ventana, self.main)
 		self.main.frameAdmin = go
@@ -220,10 +274,6 @@ class frameAdmin (CTkFrame):
 			relwidth=1, relheight=1
 		)
 		
-
 	def cerrar(self):
-		self.main.frameAdmin.place_forget()
-		self.main.frameLogin.place(
-            relx=0, rely=0,
-            relwidth=1, relheight=1
-        )
+		self.removerfunciones()
+		self.main.mostrar_login()
